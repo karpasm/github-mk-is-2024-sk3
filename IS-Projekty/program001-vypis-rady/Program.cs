425fdb5240104255120dd742029c3bf3ab3f65e1
Matyáš Karpaš <matyas.karpas@tul.cz>
2024-10-03 17:49:07 +0200

    #1 první program - uživatelský vstup

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+            Console.Clear();  //vymaže konzoli
+            Console.WriteLine("***Výpis číselné řady *****");
+            Console.WriteLine("***Matyáš Karpaš **********");
+            Console.WriteLine("***************************");  //napíše mi danou věc na vlastní řádek
+            Console.WriteLine(" ");
+
+            //vstup od uživatele
+
+           /* Console.Write("zadejte první číslo: ");
+            int first = int.Parse(Console.ReadLine()); */
+
+            //vstup od uživatele lépe řešený
+            Console.Write("zadejte číslo: ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {    //dokud uživatel nezadá číslo správně, bude se furt oběvovat console.write(nezadali...)
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: "); 
+                }
+
+            Console.ReadKey();  //díky tomu bude čekat konzole na zavření, jinak by se sama zavřela
+
+    }
+}
