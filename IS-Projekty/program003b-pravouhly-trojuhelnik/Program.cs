dc6d42d9de93a7eefd9236716e0d43c60b7f4905
karpasm <matyas.karpas@tul.cz>
2024-11-02 12:39:31 +0100

    ukol 003-b

---
+++ IS-Projekty/program003b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace program003b_pravouhly_trojuhelnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte velikost pravoúhlého trojúhelníku: ");
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
